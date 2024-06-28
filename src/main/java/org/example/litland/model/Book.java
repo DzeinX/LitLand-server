50dff76aff4aed4d639d1e8f8840d0faabba16e0
Пронин Вадим Викторович <proninvv@vogu35.ru>
2024-06-28 21:45:13 +0300

    Расширение типа varchar и проставление обязательных параметров в модели книги

--- src/main/java/org/example/litland/model/Book.java
+++ src/main/java/org/example/litland/model/Book.java
@@ -13,26 +13,35 @@ public class Book {
     @GeneratedValue
     private Long id;
 
+    @Column(length = 255, nullable = false)
     private String name = ""; // required
 
+    @Column(length = 4, nullable = false)
     private Language language = Language.RU; // required
 
     private Integer pages;
 
+    @Column(nullable = false)
     private Float price = 0f; // required
 
+    @Column(length = 20, nullable = true)
     private String ISBNNumber;
 
     private Boolean isNew;
 
+    @Column(nullable = false)
     private Integer storageAmount = 0; // required
 
+    @Column(length = 65535)
     private String description = "";
 
+    @Column(length = 20, nullable = false)
     private String fileName;
 
+    @Column(length = 20, nullable = false)
     private String coverName;
 
+    @Column(nullable = false)
     private Integer publicationYear = Calendar.getInstance().get(Calendar.YEAR); // required
 
     private Float rating;
@@ -41,6 +50,7 @@ public class Book {
 
     private String genre;
 
+    @Column(nullable = false)
     private String authors = ""; // required
 
 //    @ManyToOne(targetEntity = Publisher.class)
